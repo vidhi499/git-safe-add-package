cdd6d1af928153f31835db693446412d1e22bf27
Vidhi Kataria <katariavidhi99@gmail.com>
2024-01-27 13:15:28 +0530

    fixes

--- awesome-package/index.js
+++ awesome-package/index.js
@@ -7,12 +7,26 @@ const packageName = args[1];
 const currDir = process.cwd();
 const sourcePath = path.join(currDir, "temp");
 const ignoredFiles = ["script.js", `${packageName}`, "node_modules"];
+let branchName, originBranchName, subOrigin;
 
 function getAllFiles() {
   const filenames = fs.readdirSync(__dirname);
-
-  console.log("\nCurrent directory filenames:");
   return filenames;
+}
+
+function generateRandomString() {
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let randomString = "";
+
+  for (let i = 0; i < 6; i++) {
+    // Generate a random index to pick a character from the 'characters' string
+    const randomIndex = Math.floor(Math.random() * characters.length);
+
+    // Append the selected character to the random string
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
 }
 
 function cloneGitRepo() {
@@ -33,8 +47,6 @@ function cloneGitRepo() {
 
 function getAllFiles() {
   const filenames = fs.readdirSync(sourcePath);
-
-  console.log("\nCurrent directory filenames:");
   return filenames;
 }
 
@@ -48,7 +60,6 @@ function createPackageBranch() {
   files.forEach((file) => {
     if (!ignoredFiles.includes(file)) {
       const tempSourcePath = path.join(sourcePath, file);
-      console.log(file, tempSourcePath, destPath);
       var child = exec(
         `cd ${sourcePath} && git mv ${tempSourcePath} ${destPath}`,
         function (err, stdout, stderr) {
@@ -57,16 +68,66 @@ function createPackageBranch() {
           } else if (typeof stderr != "string") {
             return new Error(stderr);
           } else {
+            // TODO: Check if all folders are copied or not
+            createBranchAndPush();
             return stdout;
           }
         }
       );
     }
   });
+  // TODO: Just for development
+  createBranchAndPush();
+}
+
+function fetchPackage() {
+  originBranchName = `${generateRandomString()}-origin-branch`;
+  subOrigin = `${generateRandomString()}-origin`;
+  console.log(originBranchName);
+  exec(
+    // TODO: Make branchName dynamic
+    `git remote add ${subOrigin} ./temp && git fetch ${subOrigin} && git branch ${originBranchName} remotes/${subOrigin}/dchDsI `,
+    function (err, stdout, stderr) {
+      if (err != null) {
+        console.log(JSON.stringify(err), "ERROR");
+        return new Error(err);
+      } else if (typeof stderr != "string") {
+        onsole.log(JSON.stringify(stderr), "STDERR");
+        return new Error(stderr);
+      } else {
+        // exec(`cd ${source} &&  git push origin ${branchName}`);
+        return stdout;
+      }
+    }
+  );
+}
+
+function createBranchAndPush() {
+  branchName = generateRandomString();
+  console.log(branchName, sourcePath);
+  exec(
+    `cd ${sourcePath} && git checkout -b ${branchName} && git add . && git push origin ${branchName}`,
+    function (err, stdout, stderr) {
+      if (err != null) {
+        console.log(JSON.stringify(err), "ERRR");
+        return new Error(err);
+      } else if (typeof stderr != "string") {
+        onsole.log(JSON.stringify(stderr), "STDERR");
+        return new Error(stderr);
+      } else {
+        fetchPackage();
+        // exec(`cd ${source} &&  git push origin ${branchName}`);
+        return stdout;
+      }
+    }
+  );
 }
 
 function main() {
+  // TODO: Add error for existing changes
+  // Get current branch
   // cloneGitRepo();
-  createPackageBranch();
+  // createPackageBranch();
+  fetchPackage();
 }
 main();
